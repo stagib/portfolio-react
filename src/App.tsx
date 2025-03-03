5cdd3ee431a28bc3792b75ccb0d7b4cf14a2bf28
stagib <147700418+stagib@users.noreply.github.com>
2025-03-03 16:54:04 +1100

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-    return <div>Hello World</div>;
+    return (
+        <div className="flex flex-col items-center">
+            <div className="flex w-full max-w-5xl justify-between p-4 sm:p-8">
+                <div>
+                    <div className="mb-1 text-2xl font-semibold">Song Tran</div>
+                    <div className="text-stone-400">Full stack developer</div>
+                    <div className="text-xs text-stone-400">
+                        Melbourne, Australia
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
